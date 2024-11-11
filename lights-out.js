41618276e7b1058a3082f42a6c4f2816414bf9ff
Cameron-Taleghani <ctcode47368@protonmail.com>
2024-11-11 13:01:22 -0700

    Randomize/complete (#3)
    * First test for HW 3
    
    * Added randomize method
    
    * Deleted test.txt file manually
    
    * Applied some style changes to lights-out.js

--- lights-out.js
+++ lights-out.js
@@ -1,7 +1,19 @@
-class LightsOut1 {
+class LightsOut {
     constructor(gameId) {
         this.gameId = gameId;
         this.numRows = 4;
         this.numCols = 4;
+        this.randomize();
+    }
+
+    randomize() {
+        for(let row = 0; row < this.numRows; row++) {
+            for(let col = 0; col < this.numCols; col++) {
+                if(Math.random() < 0.5) {
+                    document.querySelector(`#${this.gameId} [data-row="${row}"][data-col="${col}"]`)
+                    .classList.toggle("on");
+                }
+            }
+        }
     }
 }
