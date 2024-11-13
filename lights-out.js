67021f725c6c66c725bd307159395c1f61e9bc4c
Cameron Taleghani <ctcode47368@protonmail.com>
2024-11-13 10:05:48 -0700

    Added getNeighbors() and registerClicks() to lights-out.js

--- lights-out.js
+++ lights-out.js
@@ -4,6 +4,7 @@ class LightsOut {
         this.numRows = 4;
         this.numCols = 4;
         this.randomize();
+        this.registerClicks();
     }
 
     randomize() {
@@ -13,6 +14,53 @@ class LightsOut {
                     document.querySelector(`#${this.gameId} [data-row="${row}"][data-col="${col}"]`)
                     .classList.toggle("on");
                 }
+            }
+        }
+    }
+
+    getNeighbors(row, col) {
+        const neighborArray = [];
+        
+        if(row < this.numRows - 1) {
+            if(row > 0) {
+                neighborArray.push([row + 1, col]);
+                neighborArray.push([row - 1, col]);
+            } else {
+                neighborArray.push([row + 1, col]);
+                neighborArray.push([this.numRows - 1, col]);
+            }
+        } else {
+            neighborArray.push([0, col]);
+            neighborArray.push([row - 1, col]);
+        }
+        if(col < this.numCols - 1) {
+            if(col > 0) {
+                neighborArray.push([row, col + 1]);
+                neighborArray.push([row, col - 1]);
+            } else {
+                neighborArray.push([row, col + 1]);
+                neighborArray.push([row, this.numCols - 1]);     
+            }
+        } else {
+            neighborArray.push([row, 0]);
+            neighborArray.push([row, col - 1]);
+        }
+
+        console.log(neighborArray);
+        return neighborArray;
+    }
+
+    registerClicks() {
+        for(let row = 0; row < this.numRows; row++) {
+            for(let col = 0; col < this.numCols; col++) {
+                const button = document.querySelector(`#${this.gameId} [data-row="${row}"][data-col="${col}"]`);
+                button.addEventListener('click', () => {
+                    button.classList.toggle("on");
+                    const neighbors = this.getNeighbors(row, col);
+                    
+                    neighbors.forEach(neighbor => document.querySelector(`#${this.gameId} [data-row="${neighbor[0]}"][data-col="${neighbor[1]}"]`)
+                    .classList.toggle("on"));
+                });
             }
         }
     }
