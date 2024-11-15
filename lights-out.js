4c10c0f1ad917767e60bec88baaad131225d270c
Cameron Taleghani <ctcode47368@protonmail.com>
2024-11-14 17:17:51 -0700

    Added solver() method and made some style changes

--- lights-out.js
+++ lights-out.js
@@ -5,6 +5,7 @@ class LightsOut {
         this.numCols = 4;
         this.randomize();
         this.registerClicks();
+        this.solve();
     }
 
     randomize() {
@@ -28,8 +29,7 @@ class LightsOut {
                 neighborArray.push([this.numRows - 1, col]);
             }
         } else {
-            neighborArray.push([0, col]);
-            neighborArray.push([row - 1, col]);
+            neighborArray.push([0, col]), neighborArray.push([row - 1, col]);
         }
         if (col < this.numCols - 1) {
             neighborArray.push([row, col + 1]);
@@ -39,8 +39,7 @@ class LightsOut {
                 neighborArray.push([row, this.numCols - 1]);     
             }
         } else {
-            neighborArray.push([row, 0]);
-            neighborArray.push([row, col - 1]);
+            neighborArray.push([row, 0]), neighborArray.push([row, col - 1]);
         }
         return neighborArray;
     }
@@ -55,9 +54,44 @@ class LightsOut {
                     for (const neighbor of neighbors) {
                         const [r, c] = neighbor;
                         document.querySelector(`#${this.gameId} [data-row="${r}"][data-col="${c}"]`)
-                            .classList.toggle("on")
+                            .classList.toggle("on");
+                    }
+                    if (!button.textContent) {
+                        this.solve();
+                    } else {
+                        button.textContent = null;
                     }
                 });
+            }
+        }
+    }
+
+    solve() {
+        for (let row = 0; row < this.numRows; row++) {
+            for (let col = 0; col < this.numCols; col++) {
+                document.querySelector(`#${this.gameId} [data-row="${row}"][data-col="${col}"]`).textContent = null;
+            }
+        }
+        for (let row = 0; row < this.numRows; row++) {
+            for (let col = 0; col < this.numCols; col++) {
+                const button = document.querySelector(`#${this.gameId} [data-row="${row}"][data-col="${col}"]`);
+                const neighbors = this.getNeighbors(row, col);
+                if (button.classList.contains("on")) { 
+                    if (button.textContent) {
+                        button.textContent = null;
+                    } else {
+                        button.textContent = "Click me!";
+                    }
+                    const neighbors = this.getNeighbors(row, col);
+                    for (const neighbor of neighbors) {
+                        const [r, c] = neighbor;
+                        if (document.querySelector(`#${this.gameId} [data-row="${r}"][data-col="${c}"]`).textContent) {
+                            document.querySelector(`#${this.gameId} [data-row="${r}"][data-col="${c}"]`).textContent = null;
+                        } else {
+                            document.querySelector(`#${this.gameId} [data-row="${r}"][data-col="${c}"]`).textContent = "Click me!";
+                        }
+                    }
+                }
             }
         }
     }
